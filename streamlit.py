d7ad67dcf72d9ec7928bbbb3ee7fcec8d06c7ee2
Alfiia Fakhrutdinova <130904844+AlfiiaF@users.noreply.github.com>
2023-04-17 16:24:11 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,6 @@ import pandas as pd
 import shap
 import matplotlib.pyplot as plt
 
-
 st.title('Kidney Stone Prediction based on Urine Analysis')
 
 st.image('Kidney.jpeg')
@@ -20,51 +19,17 @@ st.markdown("""
       ***
 """)
 
-
 st.sidebar.header("Specify input parameters")
 
-
-
 GRAVITY = st.sidebar.slider('GRAVITY', float(X.gravity.min()), float(X.gravity.max()), float(X.gravity.mean()))
+
 PH = st.sidebar.slider('PH', float(X.ph.min()), float(X.ph.max()), float(X.ph.mean()))
+
 OSMO = st.sidebar.slider('OSMO', float(X.osmo.min()), float(X.osmo.max()), float(X.osmo.mean()))
+
 COND = st.sidebar.slider('COND', float(X.cond.min()), float(X.cond.max()), float(X.cond.mean()))
+
 UREA = st.sidebar.slider('UREA', float(X.urea.min()), float(X.urea.max()), float(X.urea.mean()))
+
 CALC = st.sidebar.slider('CALC', float(X.calc.min()), float(X.calc.max()), float(X.calc.mean()))
 
-data = {'gravity': GRAVITY,
-      'ph': PH,
-      'osmo': OSMO,
-      'cond': COND, 
-      'urea': UREA,
-      'calc': CALC}
-
-features = pd.DataFrame(data, index=[0])
-
-st.header('Specified input parameters')
-st.write(features)
-st.write('---')
-
-model = joblib.load("model.pkl")
-
-prediction = model.predict(df)
-
-
-st.header('Prediction of Kidney Stone')
-st.write(prediction)
-if prediction == 0:
-  st.write("You don't have kidney stones")
-else:
-  st.write("You have kidney stones. See a doctor")
-
-st.write('---')
-
-
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
-
-st.header('Feature Importance')
-plt.title('Feature importance based on SHAP values')
-shap.summary_plot(shap_values, X)
-st.pyplot(bbox_inches='tight')
-st.write('---')
