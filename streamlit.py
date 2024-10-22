e6076350ad4cd2444748c8ed851e871b67d7deb9
Alfiia Fakhrutdinova <130904844+AlfiiaF@users.noreply.github.com>
2024-10-22 16:55:20 +0300

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,71 +1,77 @@
 import joblib
 import streamlit as st
 import pandas as pd
+import base64
+import pickle
 from PIL import Image
-import matplotlib.pyplot as plt
+from urllib.request import urlopen
 import shap
 
-# Загрузка изображения
 image = Image.open('Kidney.jpeg')
 
-st.title('Kidney Stone Prediction based on Urine Analysis')
+st.title('# Kidney Stone Prediction based on Urine Analysis')
 
 st.image(image, use_column_width=True)
 
-# Описание
 st.markdown("""
-      The six physical characteristics of the urine are: (1) specific gravity...
-      ***
+The six physical characteristics of the urine are: (1) specific gravity, the density of the urine relative to water; (2) pH, the negative logarithm of the hydrogen ion; (3) osmolarity (mOsm), a unit used in biology and medicine but not in
+physical chemistry. Osmolarity is proportional to the concentration of
+molecules in solution; (4) conductivity (mMho milliMho). One Mho is one
+reciprocal Ohm. Conductivity is proportional to the concentration of charged
+ions in solution; (5) urea concentration in millimoles per litre; and (6) calcium
+concentration (CALC) in millimolesllitre.
+The data is obtained from 'Physical Characteristics of Urines With and Without Crystals',a chapter from Springer Series in Statistics.
+***
 """)
+
 
 st.sidebar.header("Specify input parameters")
 
-# Функция для получения пользовательского ввода
-def user_input_features(X):
-  GRAVITY = st.sidebar.slider('GRAVITY', float(X.gravity.min()), float(X.gravity.max()), float(X.gravity.mean()))
-  PH = st.sidebar.slider('PH', float(X.ph.min()), float(X.ph.max()), float(X.ph.mean()))
-  OSMO = st.sidebar.slider('OSMO', float(X.osmo.min()), float(X.osmo.max()), float(X.osmo.mean()))
-  COND = st.sidebar.slider('COND', float(X.cond.min()), float(X.cond.max()), float(X.cond.mean()))
-  UREA = st.sidebar.slider('UREA', float(X.urea.min()), float(X.urea.max()), float(X.urea.mean()))
-  CALC = st.sidebar.slider('CALC', float(X.calc.min()), float(X.calc.max()), float(X.calc.mean()))
+def user_input_features():
+GRAVITY = st.sidebar.slider('GRAVITY', float(X.gravity.min()), float(X.gravity.max()), float(X.gravity.mean()))
+PH = st.sidebar.slider('PH', float(X.ph.min()), float(X.ph.max()), float(X.ph.mean()))
+OSMO = st.sidebar.slider('OSMO', float(X.osmo.min()), float(X.osmo.max()), float(X.osmo.mean()))
+COND = st.sidebar.slider('COND', float(X.cond.min()), float(X.cond.max()), float(X.cond.mean()))
+UREA = st.sidebar.slider('UREA', float(X.urea.min()), float(X.urea.max()), float(X.urea.mean()))
+CALC = st.sidebar.slider('CALC', float(X.calc.min()), float(X.calc.max()), float(X.calc.mean()))
 
-  data = {'gravity': GRAVITY,
-          'ph': PH,
-          'osmo': OSMO,
-          'cond': COND, 
-          'urea': UREA,
-          'calc': CALC}
+data = {'gravity': GRAVITY,
+'ph': PH,
+'osmo': OSMO,
+'cond': COND,
+'urea': UREA,
+'calc': CALC}
 
-  features = pd.DataFrame(data, index=[0])
-  return features
+features = pd.DataFrame(data, index=[0])
+return features
 
-# Загрузка модели и данных
-model = joblib.load("model.pkl")
-X = pd.read_csv('train.csv') # Предполагается, что у вас есть файл данных 'data.csv'
-
-df = user_input_features(X)
+df = user_input_features()
+print(df.head())
 
 st.header('Specified input parameters')
 st.write(df)
 st.write('---')
 
-# Предсказание
-prediction = model.predict(df)
+# model = joblib.load("model.pkl")
 
-st.header('Prediction of Kidney Stone')
-if prediction == 0:
-  st.write("You don't have kidney stones.")
-else:
-  st.write("You have kidney stones. See a doctor.")
+# prediction = model.predict(df)
 
-st.write('---')
 
-# Важность признаков
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
+# st.header('Prediction of Kidney Stone')
+# st.write(prediction)
+# if prediction == 0:
+# st.write("You don't have kidney stones")
+# else:
+# st.write("You have kidney stones. See a doctor")
 
-st.header('Feature Importance')
-fig, ax = plt.subplots()
-shap.summary_plot(shap_values, X, plot_type="bar", show=False)
-st.pyplot(fig)
-st.write('---')
+# st.write('---')
+
+
+# explainer = shap.TreeExplainer(model)
+# shap_values = explainer.shap_values(X)
+
+# st.header('Feature Importance')
+# plt.title('Feature importance based on SHAP values')
+# shap.summary_plot(shap_values, X)
+# st.pyplot(bbox_inches='tight')
+# st.write('---')
